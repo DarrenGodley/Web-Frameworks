9abe9931b43effbb72b0990909c8957726784b2d
Darren Godley <darren.godley@students.ittralee.ie>
2018-10-18 10:50:56 +0100

    second commit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,14 +2,16 @@
 const homelist = function(req, res){
     res.render('locations-list', { title: 'Home' });
 };
+
 /* GET 'Location info' page */
 const locationInfo = function(req, res){
     res.render('location-info', { title: 'Location info' });
 };
-/* GET 'Add review' page */
+
 const addReview = function(req, res){
-    res.render('index', { title: 'Add review' });
+    res.render('location-review-form', { title: 'Add review' });
 };
+
 module.exports = {
     homelist,
     locationInfo,
